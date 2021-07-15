a855bdfe3727b6208f91bd83359564f555db6538
KellerBreno <brenok.private@gmail.com>
2021-07-15 16:42:12 -0300

    Adicionado popup de aviso e seleção de lesoes

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import shutil
 
+import pandas as pd
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.properties import ListProperty
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
 from plyer import filechooser
 
@@ -22,25 +24,34 @@ class AppManager(ScreenManager):
     pass
 
 
+def _get_result(img_name):
+    name = img_name[:-4]
+    df = pd.read_csv(BASE_PATH + BASE_ANALYSIS_PATH + "labels/" + name + ".txt", sep=" ",
+                     names=["lesion", "x", "y", "width", "heigth"])
+    df.sort_values(by=["lesion"], inplace=True)
+    if df.iloc[0]["lesion"] == 0:
+        return "Essa imagem foi classificada como: LESIONADA"
+    else:
+        return "Essa imagem foi classificada como: NEGATIVA"
+
+
 class HomeScreen(Screen):
     selection = ListProperty([])
-
-    def load_txt(self):
-        self.manager.ids.view_screen.ids.lb.text = self.ids.txt.text
-        self.manager.current = 'view_screen'
-        self.ids.txt.text = ''  # para limpar o input quando voltar pra essa tela
 
     def _set_image(self):
         splits = self.ids.image.source.split("\\")
         img_name = splits[len(splits) - 1]
-        print(BASE_PATH + BASE_ANALYSIS_PATH + img_name)
         self.manager.ids.view_screen.ids.image2.source = BASE_PATH + BASE_ANALYSIS_PATH + img_name
+        self.manager.ids.view_screen.ids.img_result_text.text = _get_result(img_name)
+        self.popup.dismiss()
 
     def submit_img(self):
+        self.manager.current = 'view_screen'
+        self.popup = WaitingPopUp()
+        self.popup.open()
         detect_thread = DetectThread(self.ids.image.source, self._set_image)
         detect_thread.daemon = True
         detect_thread.start()
-        self.manager.current = 'view_screen'
 
     def selected(self, file):
         try:
@@ -56,6 +67,7 @@ class HomeScreen(Screen):
 
     def on_selection(self, *a, **k):
         self.ids.image.source = self.selection[0]
+        self.ids.submit_button.disabled = False
 
     def close_application(self):
         App.get_running_app().stop()
@@ -67,6 +79,24 @@ class ViewScreen(Screen):
         self.manager.current = 'home_screen'
         _clean_environment()
 
+    def change_original(self):
+        self.manager.ids.view_screen.ids.image2.source = self.manager.ids.home_screen.ids.image.source
+
+    def change_geral(self):
+        splits = self.manager.ids.home_screen.ids.image.source.split("\\")
+        img_name = splits[len(splits) - 1]
+        self.manager.ids.view_screen.ids.image2.source = BASE_PATH + BASE_ANALYSIS_PATH + img_name
+
+    def change_lesioned(self):
+        splits = self.manager.ids.home_screen.ids.image.source.split("\\")
+        img_name = splits[len(splits) - 1]
+        self.manager.ids.view_screen.ids.image2.source = BASE_PATH + LESIONED_ANALYSIS_PATH + img_name
+
+    def change_negative(self):
+        splits = self.manager.ids.home_screen.ids.image.source.split("\\")
+        img_name = splits[len(splits) - 1]
+        self.manager.ids.view_screen.ids.image2.source = BASE_PATH + NEGATIVE_ANALYSIS_PATH + img_name
+
 
 class FileChoose(Button):
     pass
@@ -76,8 +106,13 @@ class LabelDropDown(DropDown):
     pass
 
 
+class WaitingPopUp(Popup):
+    pass
+
+
 class Test(App):
     def build(self):
+        self.title = "Ferramenta XX"
         return AppManager()
 
 
